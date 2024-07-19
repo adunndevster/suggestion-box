d8ce88c4a337efa1abff48d8977e6507a042a421
Adam Dunn <adunndevster@gmail.com>
2024-07-19 14:57:21 -0600

    new suggestion button

--- src/views/components/SidePanel.tsx
+++ src/views/components/SidePanel.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Suggestion } from "../../data/data";
 import { formatDate, getUser } from "../../utils/misc";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
 interface SidePanelProps {
   suggestions: Suggestion[];
@@ -15,24 +17,34 @@ const SidePanel: React.FC<SidePanelProps> = ({
 }) => {
   return (
     <div className="side-panel">
-      {suggestions.map((suggestion) => {
-        const user = getUser(suggestion.userId);
-        const isActive = activeSuggestion && activeSuggestion.conversationId === suggestion.conversationId;
-        return (
-          <div
-            className={`suggestion-item ${isActive ? 'active' : ''}`}
-            key={suggestion.conversationId}
-            onClick={() => onSuggestionClick(suggestion)}
-          >
-            <div className="title">{suggestion.title}</div>
-            <div className="small">{formatDate(suggestion.timestamp)}</div>
-            <div className="person">
-              <span><div className="initials">{user.initials}</div></span>
-              <span className="small">{user.name}</span>
+      <div className="new-suggestion">
+        <FontAwesomeIcon icon={faPenToSquare} />
+        New Suggestion
+      </div>
+      <div className="side-panel-items">
+        {suggestions.map((suggestion) => {
+          const user = getUser(suggestion.userId);
+          const isActive =
+            activeSuggestion &&
+            activeSuggestion.conversationId === suggestion.conversationId;
+          return (
+            <div
+              className={`suggestion-item ${isActive ? "active" : ""}`}
+              key={suggestion.conversationId}
+              onClick={() => onSuggestionClick(suggestion)}
+            >
+              <div className="title">{suggestion.title}</div>
+              <div className="small">{formatDate(suggestion.timestamp)}</div>
+              <div className="person">
+                <span>
+                  <div className="initials">{user.initials}</div>
+                </span>
+                <span className="small">{user.name}</span>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
